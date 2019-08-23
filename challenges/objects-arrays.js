43f7e046daa05eab05c8b79f36cd0e8d49296ae3
Anthony Lopez <anthonybeconscious@gmail.com>
2019-08-23 11:23:26 -0500

    stuck on universities request 3. moving on

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -80,8 +80,18 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities);
+let universities = [];
+
+let uniOnly = function(arr){
+  let result = [];
+  for (let i = 0; i< arr.length; i++){
+   result.push(arr[i].university);
+  }
+  return result
+}
+universities = uniOnly(graduates)
+
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -89,11 +99,24 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
+
 const contactInfo = [];
+
+for (let i = 0; i < graduates.length; i++){
+  contactInfo.push(graduates[i].first_name + " " + graduates[i].email);
+}
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
+
+
 const unisWithUni = [];
+
+for (let i = 0; i< graduates.length; i++){
+  if(graduates[i].university === 'University'){
+   unisWithUni.push(graduates[i]);
+  }
+}
 console.log(unisWithUni);
 
 
